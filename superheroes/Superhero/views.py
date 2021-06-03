8f9be37e6b13698e94f9dfe0830f847ab7e7e933
Austin Flatt <austindflatt@gmail.com>
2021-06-03 11:11:42 -0400

    fixed programming error when adding superhero to database  in admin panel

--- superheroes/Superhero/views.py
+++ superheroes/Superhero/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import SuperHeroes
 
 # Create your views here.
 
 
 def index(request):
-    return  render(request, 'Superhero/index.html')
+    all_superheros = SuperHeroes.objects.all()
+    return render(request, 'Superhero/index.html')
