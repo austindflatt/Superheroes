12f3e91a8256f796f63c7a08d235788fc7d4f59d
Austin Flatt <austindflatt@gmail.com>
2021-06-03 14:36:50 -0400

    fixed details page for superhero and it is fully functional

--- superheroes/Superhero/urls.py
+++ superheroes/Superhero/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 app_name = 'Superhero'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:SuperHeroes_id>/', views.detail, name='detail'),
+    path('<int:superhero_id>/', views.detail, name='detail'),
     path('new/', views.create, name='create_new_superhero')
 ]
