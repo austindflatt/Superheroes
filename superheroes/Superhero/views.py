038d0306c329f08a5f4a8c0a3486cabfebf8cd25
Austin Flatt <austindflatt@gmail.com>
2021-06-03 14:08:17 -0400

    finished tutorial, still need to fix view superhero detail page

--- superheroes/Superhero/views.py
+++ superheroes/Superhero/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import SuperHeroes
+from django.urls import reverse
 
 # Create your views here.
 
@@ -19,3 +20,16 @@ def detail(request, superhero_id):
         'hero': hero
     }
     return render(request, 'Superhero/details.html', context)
+
+def create(request):
+    if request.method == 'POST':
+        superhero_name = request.POST.get('superhero_name')
+        alter_ego_name = request.POST.get('alter_ego_name')
+        primary_ability = request.POST.get('primary_ability')
+        secondary_ability = request.POST.get('secondary_ability')
+        catchphrase = request.POST.get('secondary_ability')
+        new_superhero = SuperHeroes(superhero_name=superhero_name, alter_ego_name=alter_ego_name, primary_ability=primary_ability, secondary_ability=secondary_ability, catchphrase=catchphrase)
+        new_superhero.save()
+        return HttpResponseRedirect(reverse('Superhero:index'))
+    else:
+        return render(request, 'Superhero/create.html')
